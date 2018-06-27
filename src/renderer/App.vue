31d6f62302fc5435e1a12fd9586bb32c54785c8c
nagaminenot <nagamine@efg.company>
2018-06-27 12:02:29 +0900

    vue initした
    vue initまで

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'project_c'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
